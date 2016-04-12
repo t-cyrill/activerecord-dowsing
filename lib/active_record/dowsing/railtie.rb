98116e72bda5bc92c0f8e95cd8ec9aba4d124f34
cyrill <siril.taka@gmail.com>
2016-04-12 16:00:42 +0900

    Workaround support config.x parameter

--- lib/active_record/dowsing/railtie.rb
+++ lib/active_record/dowsing/railtie.rb
@@ -4,8 +4,8 @@ module ActiveRecord
       config.activerecord_dowsing = ActiveSupport::OrderedOptions.new
 
       initializer 'ActiveRecord::Dowsing patch' do
-        if Rails.configuration.activerecord_dowsing.enabled
-          || Rails.configuration.x.activerecord_dowsing.enabled
+        if Rails.configuration.x.activerecord_dowsing.try(:enabled) \
+          || Rails.configuration.activerecord_dowsing.try(:enabled)
           ActiveSupport.on_load :active_record do
             ActiveRecord::ConnectionAdapters::Mysql2Adapter.module_eval do
               include ActiveRecord::Dowsing::Patch
